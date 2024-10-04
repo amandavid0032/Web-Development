b15e09f53fb8e66974ff32aba19f5586fbee940d
Aman David <86879390+amandavid0032@users.noreply.github.com>
2024-10-04 10:45:15 +0530

    Update React.js

--- React.js
+++ React.js
@@ -1,1 +1,10 @@
+## 📔 ES5
+---
+## 📘 Developer Responsibility
 
+---
+## 📘 Javascript
+**What is React**
+React is a JavaScript library that allows developers to build user interfaces (UIs) for web and mobile applications
+
+React is primarily used to build web applications, but it can also be used to create mobile and desktop apps, images, PDFs, and more
